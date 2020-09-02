6e4ed760e4a611247906c1378e9d7efaf74e63db
Miguel Alejandro Lopez Jimenez <miguel.alejnd@gmail.com>
2020-09-02 11:26:57 -0600

    He creado los modelos de la aplic.

--- incapp/models.py
+++ incapp/models.py
@@ -1,3 +1,44 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Departamento(models.Model):
+    id = models.IntegerField(primary_key=True)
+    nombre = models.CharField(max_length=50)
+
+class Municipio(models.Model):
+    id = models.IntegerField(primary_key=True)
+    nombre = models.CharField(max_length=50)
+    departamento = models.ForeignKey(to=Departamento, on_delete=models.CASCADE)
+
+class Canton(models.Model):
+    id = models.IntegerField(primary_key=True)
+    nombre = models.CharField(max_length=50)
+    municipio = models.ForeignKey(to=Municipio, on_delete=models.CASCADE)
+
+
+class Direccion(models.Model):
+    departamento = models.ForeignKey(to=Departamento, on_delete=models.CASCADE)
+    municipio = models.ForeignKey(to=Municipio, on_delete=models.CASCADE)
+    canton = models.ForeignKey(to=Canton, null=True, blank=True)
+    
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=30)
+
+class Problema(models.Model):
+    origen = models.ForeignKey(to=Direccion)
+    destino = models.ForeignKey(to=Direccion)
+    categoria = models.ForeignKey(
+        to=Categoria,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+    )
+    usuario = models.ForeignKey(
+        to=Usuario,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+    )
+    descripcion = models.CharField(max_length=200)
+    fecha = models.DateField(auto_now_add=True)
