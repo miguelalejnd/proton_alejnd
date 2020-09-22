471461c9072a048540cd234ca28f6436d0b1f4d0
Miguel Alejandro Lopez Jimenez <miguel.alejnd@gmail.com>
2020-09-22 10:25:36 -0600

    Mejorando en algunos detalles los modelos de la aplicacion incapp

--- incapp/models.py
+++ incapp/models.py
@@ -9,25 +9,34 @@ class Departamento(models.Model):
 class Municipio(models.Model):
     id = models.IntegerField(primary_key=True)
     nombre = models.CharField(max_length=50)
-    departamento = models.ForeignKey(to=Departamento, on_delete=models.CASCADE)
+    departamento = models.ForeignKey(
+        to=Departamento,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True
+    )
 
 class Canton(models.Model):
     id = models.IntegerField(primary_key=True)
     nombre = models.CharField(max_length=50)
-    municipio = models.ForeignKey(to=Municipio, on_delete=models.CASCADE)
-
+    municipio = models.ForeignKey(
+        to=Municipio,
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True
+    )
 
 class Direccion(models.Model):
     departamento = models.ForeignKey(to=Departamento, on_delete=models.CASCADE)
     municipio = models.ForeignKey(to=Municipio, on_delete=models.CASCADE)
     canton = models.ForeignKey(to=Canton, null=True, blank=True)
-    
+
 class Categoria(models.Model):
     nombre = models.CharField(max_length=30)
 
 class Problema(models.Model):
-    origen = models.ForeignKey(to=Direccion)
-    destino = models.ForeignKey(to=Direccion)
+    origen = models.ForeignKey(to=Direccion, help_text="La dirección de su origen.")
+    destino = models.ForeignKey(to=Direccion, help_text="La dirección de su destino.")
     categoria = models.ForeignKey(
         to=Categoria,
         on_delete=models.SET_NULL,
